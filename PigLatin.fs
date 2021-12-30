d184456fe142cf930582d3bab6c055c28b194a7f
jinyus <starboymc4@gmail.com>
2021-12-30 13:22:14 -0500

    refactor

--- PigLatin.fs
+++ PigLatin.fs
@@ -7,26 +7,27 @@ let startWithVowel (str: string) : bool = Regex.IsMatch(str, "^(xr|yt|[aeiou])")
 
 let yEdgeCase (str: string) : bool = str.Length = 2 && (str.EndsWith "y")
 
-let quEdgeCase (str: string) : bool = str.StartsWith "qu"
-
 // chair 2(index of firstvowel) -> airch
 let movePrefixToSuffix (index: int) (str: string) : string =
     let prefix = str.Substring(0, index)
     let rest = str.Substring index
     sprintf "%s%s" rest prefix
 
+let movePrefixToSuffix2 index (str: string) =
+    $"{str.Substring index}{str.Substring(0, index)}"
+
 let piglatinize (str: string) : string = str + "ay"
 
 let translate (input: string) =
     let rec inner (start: bool) (str: string) =
         if start && yEdgeCase str then
-            piglatinize (str |> movePrefixToSuffix 1)
+            str |> movePrefixToSuffix 1 |> piglatinize
         elif not start && (str.StartsWith "y") then
-            piglatinize str
-        elif quEdgeCase str then
-            piglatinize (str |> movePrefixToSuffix 2)
+            str |> piglatinize
+        elif str.StartsWith "qu" then
+            str |> movePrefixToSuffix 2 |> piglatinize
         elif startWithVowel str then
-            piglatinize str
+            str |> piglatinize
         else
             inner false (str |> movePrefixToSuffix 1)
 
