adcc00e698c4d94f91ea98f98fc9c05f395c4e78
jinyus <starboymc4@gmail.com>
2021-12-31 13:58:56 -0500

    modify testgen

--- fsunit_to_fuchu.py
+++ fsunit_to_fuchu.py
@@ -1,24 +1,27 @@
 
 import re
 
-fact_re = r'\[<Fact\((.*?)\]'
+fact_re = r'\[<Fact.*?\]'
 list_arg_re = r'= \[(.*?)\]'
+string_arg_re = r'(decode|encode) (".*?")'
 second_arg_re = r'(".*?") \|>'
-expected_re = r'equal \[(.*?)\]|Empty'
+# expected_re = r'equal \[(.*?)\]|Empty'
+expected_re = r'equal (".*?")'
 desc_re = r'let "(.*?)"'
-method_name = 'findAnagrams'
+method_name = 'decode'
+file_name = 'AtbashCipherTests'
 
-test_head = """
-module PhoneNumberTests =
+test_head = f"""
+module {file_name} =
     open Fuchu
-    open PhoneNumber
+    open AtbashCipher
 
 
     let suite =
         testList
-            "PhoneNumber Suite"
+            "AtbashCipher Suite"
             [ testList
-                "PhoneNumber Clean tests"
+                "AtbashCipher Encode tests"
                 [ 
 """
 
@@ -33,29 +36,33 @@ test_case = """
                 <| fun _ -> Assert.Equal("", {expected}, {actual}
 """
 
-with open('AnagramTest.fs') as inp:
+with open('test.fs') as inp:
     text = inp.read()
     text = re.sub(fact_re, "", text)
-    test_cases = re.split(r'\n{2}', text)
+    test_cases = re.split(r'\n{2,}', text)
     for c in test_cases:
         print(c)
         # continue
         desc = re.search(desc_re, c).group(1)
         expected_match = re.search(expected_re, c)
         if 'equal' in expected_match.group(0):
-            expected = f'[{expected_match.group(1)}]'
+            expected = expected_match.group(1)
         else:
-            expected = '[]'
+            expected = '""'
 
-        list_arg = f'[{re.search(list_arg_re, c).group(1)}]'
-        second_arg = re.search(second_arg_re, c).group(1)
+        # list_arg = f'[{re.search(list_arg_re, c).group(1)}]'
+        string_arg_match = re.search(string_arg_re, c)
+        method_name = string_arg_match.group(1)
+        string_arg = string_arg_match.group(2)
+        # second_arg = re.search(second_arg_re, c).group(1)
 
-        actual = f'{method_name} {list_arg} {second_arg})'
+        # actual = f'{method_name} {list_arg} {second_arg})'
+        actual = f'{method_name} {string_arg} )'
 
         new_test_case = test_case.replace('{description}', desc).replace(
             '{expected}', expected).replace('{actual}', actual)
         test_head += new_test_case
     test_head += test_tail
 
-    with open('GenTest.fs', 'w') as out:
+    with open(f'{file_name}.fs', 'w') as out:
         out.write(test_head)
