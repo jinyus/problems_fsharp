d2459446e795bcfc466abf0f54bbb9f8de7804e9
jinyus <starboymc4@gmail.com>
2022-01-05 00:07:05 -0500

    solved series

---
+++ Series.fs
@@ -0,0 +1,22 @@
+module Series
+
+let slices (str: string) (length: int) =
+    if length < 1 || str.Trim() = "" then
+        None
+    else
+        str
+        |> Seq.windowed length
+        |> Seq.map System.String.Concat
+        |> Seq.toList
+        |> (function
+        | [] -> None
+        | x -> Some x)
+
+
+slices "35" 2 |> printfn "%A"
+slices "9142" 2 |> printfn "%A"
+slices "12345" 6 |> printfn "%A"
+slices "12345" 0 |> printfn "%A"
+slices "" 1 |> printfn "%A"
+slices "" |> printfn "%A"
+slices "" |> printfn "%A"
